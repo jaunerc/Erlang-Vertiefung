773cf0496bf3cdf1b73db0c7839b6d9900cf9072
jaunerc <c.jauner@gmail.com>
2017-12-16 14:12:32 +0100

    hot code swapping changed.

--- src/pcp/fokus_punkte/hot_code_swapping/hcs.erl
+++ src/pcp/fokus_punkte/hot_code_swapping/hcs.erl
@@ -1,23 +1,18 @@
 -module(hcs).
--export([link_with/1, message_receiver/0, reply_with_price_of/1]).
+-export([link_with/1, answer/0]).
 
 link_with(ClientPid) ->
   register(client, ClientPid),
-  spawn_link(hcs, message_receiver, []).
+  spawn_link(hcs2, answer, []).
 
-message_receiver() ->
+answer() ->
   receive
-    {price, Item} 	-> hcs:reply_with_price_of(Item),   % hcs: added for hot code swapping
-      hcs:message_receiver()
-  end.
-
-reply_with_price_of(Item) ->
-  client ! io:format("The price of ~p is: $~p ~n", [Item, price(Item)]).
-
-price(Item) ->
-  case Item of
-    tea      -> 2.05;
-    coffee   -> 2.10;
-    milk     -> 3.5;   % price before 0.99
-    bread    -> 0.50
+    {question, _} ->
+      io:format("answer: ~p ~n", [42]),
+      ?MODULE:answer();
+    {upgrade} ->
+      code:purge(?MODULE),
+      compile:file(?MODULE),
+      code:load_file(?MODULE),
+      ?MODULE:answer()
   end.
